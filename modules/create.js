92ded5e3d9280bcc4e3a5525a7a0b43acea91ea7
irulgood <153479041+irulgood@users.noreply.github.com>
2026-05-04 16:49:16 +0700

    Update create.js

--- modules/create.js
+++ modules/create.js
@@ -147,7 +147,7 @@ Upgrade: websocket
 \`\`\`
 
 📥 *Download All Config UNLOCK SSH*:
-🔗 https://drive.google.com/drive/folders/1Yfg-r6c4RRAjcJX613G8w7I_UbTXlcS9
+🔗 https://drive.google.com/drive/folders/1Yfg-r6c4RRAjcJX613G8w7I\\_UbTXlcS9
 
 📘 *TUTORIAL GANTI SSH*
 📂 Google Drive:
